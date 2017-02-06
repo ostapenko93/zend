374d20e084eb48afe90ab8f16962204ccf6acba8
Ostapenko <ostapenko9393@gmail.com>
2017-02-06 15:49:11 +0200

    Добавил калькулятор к Мото

--- module/Moto/src/Moto/Controller/IndexController.php
+++ module/Moto/src/Moto/Controller/IndexController.php
@@ -9,6 +9,33 @@ class IndexController extends AbstractActionController
 {
     public function indexAction()
     {
-        return new ViewModel();
+        if ($_POST){
+            $price = $_POST['price'];
+            $engine = $_POST['engine'];
+            $vat = 20;
+            $duty = 10;
+            $tax = 5;
+            if($engine < 500){
+                $result['rate'] = 0.066;
+            }else{
+                $result['rate'] = 0.475;
+            }
+            $result['price'] = $price;
+            $result['vat'] = $price / 100 * $vat;
+            $result['duty'] = $price / 100 * $duty;
+            $result['excise'] = $engine * $result['rate'];
+            $result['tax'] = $price / 100 * $tax;
+            $result['certific'] = 130;
+            $result['cost'] = $result['vat'] + $result['duty'] + $result['excise'] + $result['tax'] + $result['certific'];
+            $result['amount'] = $price + $result['cost'];
+
+            //Проверка массивов
+//            echo "<pre>";
+//            print_r($result);
+//            print_r($_POST);
+//            echo "</pre>";
+
+            return $result;
+        }
     }
 }
